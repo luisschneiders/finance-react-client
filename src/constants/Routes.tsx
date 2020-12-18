fe9a8fb447e54fdb89ab32e9150c446470001ab8
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-19 06:39:52 +1100

    add constants folder for images and routes

---
+++ src/constants/Routes.tsx
@@ -0,0 +1,14 @@
+/**
+ * Constants are listed in alphabetical order
+ */
+
+export const ACCOUNT = '/account';
+export const HOME = '/home';
+export const LOGIN = '/login';
+export const LOGOUT = '/logout';
+export const REGISTER = '/register';
+export const TABS = '/tabs';
+export const TABS_EXPENSES = `${TABS}/expenses`;
+export const TABS_MAIN = `${TABS}/main`;
+export const TABS_TRANSACTIONS = `${TABS}/transactions`;
+export const WELCOME = '/welcome';

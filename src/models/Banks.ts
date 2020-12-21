961f27664de6170e9c200de4a84f87be0d73c728
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-22 06:36:18 +1100

    add model Banks

---
+++ src/models/Banks.ts
@@ -0,0 +1,11 @@
+export interface Banks {
+  bankId: number;
+  bankAccount: string;
+  bankDescription: string;
+  bankCurrentBalance: number;
+  bankInitialBalance: number;
+  bankInsertedBy: number;
+  bankIsActive: boolean;
+  bankCreatedAt: string;
+  bankUpdatedAt: string;
+}

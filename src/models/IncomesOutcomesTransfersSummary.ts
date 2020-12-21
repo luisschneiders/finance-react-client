ba0d24a3d78da031abb2bdb2ce942702e412144e
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-22 07:00:47 +1100

    code update for income outcome and transfers summary

---
+++ src/models/IncomesOutcomesTransfersSummary.ts
@@ -0,0 +1,7 @@
+export interface IncomesOutcomesTransfersSummary {
+  totalAmountByLabel: number;
+  transactionAmount: number;
+  transactionDate: string;
+  transactionFlag: string;
+  transactionLabel: string;
+}

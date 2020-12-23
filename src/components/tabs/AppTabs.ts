0b0cb6191b5302e0f6ae07f72373bf2fb86c86cc
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-23 20:16:09 +1100

    set auth and unauthenticated tabs

--- src/components/tabs/AppTabs.ts
+++ src/components/tabs/AppTabs.ts
@@ -2,18 +2,24 @@ import {
   homeOutline,
   cartOutline,
   cashOutline,
+  logInOutline,
+  starOutline,
+  happyOutline,
 } from "ionicons/icons";
 import { AppPage } from "../../models/AppPage";
 import * as ROUTES from '../../constants/Routes'
 
 export function appTabs() {
   const authenticated: AppPage[] = [
-    { url: ROUTES.TABS_MAIN, label: 'Home', icon: homeOutline },
-    { url: ROUTES.TABS_TRANSACTIONS, label: 'Transactions', icon: cashOutline },
+    { url: ROUTES.TABS_HOME, label: 'Home', icon: homeOutline },
     { url: ROUTES.TABS_EXPENSES, label: 'Expenses', icon: cartOutline },
+    { url: ROUTES.TABS_TRANSACTIONS, label: 'Transactions', icon: cashOutline },
   ];
 
   const unauthenticated: AppPage[] = [
+    { url: ROUTES.WELCOME, label: 'Welcome', icon: happyOutline },
+    { url: ROUTES.LOGIN, label: 'Login', icon: logInOutline },
+    { url: ROUTES.REGISTER, label: 'Register', icon: starOutline },
   ];
 
   const tabs: any = Object.assign({}, {

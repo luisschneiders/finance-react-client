7ee997a05f8b855ec81382361e2ef02b8d30d25e
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-24 09:34:13 +1100

    remove Promise for loadSummaryData

--- src/data/summary/data.ts
+++ src/data/summary/data.ts
@@ -2,10 +2,7 @@ import { Summary } from "../../models/Summary";
 import { getSummary } from "../api/Summary";
 
 export const loadSummaryData = async (id: number, year: number) => {
-  const response: any = await Promise.all([
-    getSummary(id, year)
-  ]);
-
+  const response: any = await getSummary(id, year);
   const summary = response as Summary;
   const data: any = {
     summary

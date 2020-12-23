0da713f669ec378013cc86f83d92005d3e763dd7
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-23 20:38:28 +1100

    update constants for routes

--- src/constants/Routes.tsx
+++ src/constants/Routes.tsx
@@ -3,12 +3,24 @@
  */
 
 export const ACCOUNT = '/account';
-export const HOME = '/home';
+
+export const DASHBOARD = '/dashboard';
+
 export const LOGIN = '/login';
 export const LOGOUT = '/logout';
+
 export const REGISTER = '/register';
+
+export const SERVER = 'http://local.developing:3030';
+
 export const TABS = '/tabs';
+export const TAB1 = `${TABS}/tab1`;
+export const TAB2 = `${TABS}/tab2`;
+export const TAB3 = `${TABS}/tab3`;
+export const TABS_HOME = `${TABS}/home`;
 export const TABS_EXPENSES = `${TABS}/expenses`;
-export const TABS_MAIN = `${TABS}/main`;
+export const TABS_TIMESHEETS = `${TABS}/timesheets`;
 export const TABS_TRANSACTIONS = `${TABS}/transactions`;
+export const TABS_TRIPS = `${TABS}/trips`;
+
 export const WELCOME = '/welcome';

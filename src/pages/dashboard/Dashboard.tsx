ab30e93f2c6be1ffc8cff70ac59f2d0da42507b4
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2020-12-23 20:17:08 +1100

    set dashboard page and style

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol
+} from '@ionic/react';
+import './Dashboard.scss';
+import * as ROUTES from '../../constants/Routes';
+
+const Dashboard: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader class="ion-text-center">
+        <IonToolbar>
+          <IonTitle>Welcome</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding ion-text-center">
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Welcome</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <div>
+                <IonButton routerLink={ROUTES.LOGIN} expand="full" color="light">Login</IonButton>
+              </div>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <span>or</span>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <div>
+                <IonButton routerLink={ROUTES.REGISTER} expand="full" color="medium">Register</IonButton>
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Dashboard;

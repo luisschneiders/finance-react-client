2ea455101091dc9b8a84d14899981c74979ad44a
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-06 21:09:09 +1100

    code refactor for summary actions

--- src/data/summary/summary.actions.ts
+++ src/data/summary/summary.actions.ts
@@ -1,16 +1,13 @@
 import { ActionType } from '../../util/types';
 import { loadSummaryData } from './data';
-import { SummaryState } from './summary.state';
 
-export const getAppSummary = (id: number, year: number) => async (dispatch: React.Dispatch<any>) => {
+export const setAppSummary = (id: number, year: number) => async (dispatch: React.Dispatch<any>) => {
   const data = await loadSummaryData(id, year);
-  dispatch(setSummaryData(data));
+  return ({
+    type: 'SET_SUMMARY_DATA',
+    data
+  } as const);
 }
 
-export const setSummaryData = (data: Partial<SummaryState>) => ({
-  type: 'SET_SUMMARY_DATA',
-  data
-} as const);
-
 export type SummaryActions =
-  | ActionType<typeof setSummaryData>
+  | ActionType<typeof setAppSummary>

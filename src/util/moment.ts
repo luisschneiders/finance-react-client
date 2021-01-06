6d1a41819315763dc465a510a668ec1cfb06dafd
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-06 21:11:38 +1100

    return current year as number

--- src/util/moment.ts
+++ src/util/moment.ts
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
 export const now = moment();
-export const currentYear = parseInt(moment().format('YYYY'));
+export const currentYear: number = parseInt(moment().format('YYYY'));

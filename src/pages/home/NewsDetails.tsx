b78c06cc48203a60995bf4b25b6fccc845f193e2
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-11 19:22:21 +1100

    add NewsDetails page

---
+++ src/pages/home/NewsDetails.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonPage,
+  IonButtons,
+  IonBackButton,
+  IonCard,
+  IonImg,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+} from '@ionic/react';
+import {
+  withRouter,
+  RouteComponentProps
+} from 'react-router';
+import './NewsDetails.scss';
+import { connect } from '../../data/connect';
+import * as ROUTES from '../../constants/Routes';
+import * as newsSelectors from '../../data/news/news.selectors';
+import { NewsCategory } from '../../models/News';
+
+interface OwnProps extends RouteComponentProps {};
+
+interface StateProps {
+  news: NewsCategory | undefined,
+};
+
+interface DispatchProps {
+};
+
+interface NewsDetailsProps extends OwnProps, StateProps, DispatchProps {};
+
+const Tab1Detail: React.FC<NewsDetailsProps> = ({
+    news,
+  }) => {
+
+  return (
+    <IonPage id="tab1-detail-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref={ROUTES.TABS_HOME}></IonBackButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonImg src={news?.image}></IonImg>
+          <IonCardHeader>
+            <IonCardTitle
+              color="tertiary"
+              className="ion-text-capitalize">{news?.category}</IonCardTitle>
+            <IonCardSubtitle>{news?.headline}</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>
+              {news?.summary} <a target="_blank" rel="noopener noreferrer" href={news?.url}>Read more...</a>
+            </p>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default connect<OwnProps, StateProps, DispatchProps>({
+  mapStateToProps: (state, OwnProps) => ({
+    news: newsSelectors.getNewsById(state, OwnProps),
+  }),
+  mapDispatchToProps: {
+  },
+  component: withRouter(Tab1Detail)
+});

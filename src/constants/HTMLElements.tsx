09fe34758b4ccac94074cc25e97cb8ec9fef926b
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-11 19:20:03 +1100

    update const for html elements

--- src/constants/HTMLElements.tsx
+++ src/constants/HTMLElements.tsx
@@ -4,3 +4,5 @@
 
 export const BANKS_CHART = 'banksChart';
 export const INCOMES_OUTCOMES_TRANSFERS_CHART = 'incomesOutcomesTransfersChart';
+export const INCOMES_OUTCOMES_CHART = 'incomesOutcomesChart';
+export const DAILY_TRANSACTIONS = 'dailyTransactions';

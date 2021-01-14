b3544dbdfad1147f5ffda03669051b1f7f4e2a3d
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-14 21:32:24 +1100

    remove component charts and const file

--- src/constants/HTMLElements.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * Constants are listed in alphabetical order
- */
-
-export const BANKS_CHART = 'banksChart';
-export const INCOMES_OUTCOMES_TRANSFERS_CHART = 'incomesOutcomesTransfersChart';
-export const INCOMES_OUTCOMES_CHART = 'incomesOutcomesChart';
-export const DAILY_TRANSACTIONS = 'dailyTransactions';

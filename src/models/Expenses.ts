41c6f3e5e7ecac545a248c109196f01e3b0a15ad
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-17 23:05:32 +1100

    add model Expenses

---
+++ src/models/Expenses.ts
@@ -0,0 +1,12 @@
+export interface Expenses {
+  expenseId: number;
+  expenseBankDescription: string;
+  expenseTypeDescription: string;
+  expenseAddress: string;
+  expenseAmount:number;
+  expenseComments: string;
+  expenseDate: string;
+  expenseTypeId: number;
+  expenseLatitude: number;
+  expenseLongitude: number;
+}

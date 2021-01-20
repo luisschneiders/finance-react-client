db926a4c5cd6392547326eb54c2770707bb81535
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-20 22:19:03 +1100

    update moment util

--- src/util/moment.ts
+++ src/util/moment.ts
@@ -1,9 +1,58 @@
+/**
+ * Replace Moment.js with javascript native features
+ */
 import moment from 'moment';
 
 export const now = moment();
-export const currentYearYYYY: number = parseInt(moment().format('YYYY'));
+export const currentYearYYYY: string = moment().format('YYYY');
+export const currentMonthYYYMMDD: string = moment().format('YYYY-MM-DD');
 
-export const monthFormatM = (date: string) => {
+export const startPeriod = (date: string) => {
+  return moment(date).startOf('month').format('YYYY-MM-DD');
+};
+export const endPeriod = (date: string) => {
+  return moment(date).endOf('month').format('YYYY-MM-DD');
+};
+
+export const subtractStartPeriod = (date: string) => {
+  return moment(date).subtract(1, 'months').startOf('month').format('YYYY-MM-DD');
+};
+export const subtractEndPeriod = (date: string) => {
+  return moment(date).subtract(1, 'months').endOf('month').format('YYYY-MM-DD');
+};
+
+export const addStartPeriod = (date: string) => {
+  return moment(date).add(1, 'months').startOf('month').format('YYYY-MM-DD');
+};
+export const addEndPeriod = (date: string) => {
+  return moment(date).add(1, 'months').endOf('month').format('YYYY-MM-DD');
+};
+
+/**
+ * Format YYYY
+ * 2021
+ */
+export const dateFormatYYYY = (date: string) => {
+  return moment(date).format('YYYY');
+};
+
+/**
+ * Format ll
+ * Jan 01, 2021
+ */
+export const dateFormatll = (date: string) => {
+  return moment(date).format('ll');
+};
+
+/**
+ * Format DD/MM/YYYY
+ * 01/01/2021
+ */
+export const dateFormatDDMMYYYY = (date: string) => {
+  return moment(date).format('DD/MM/YYYY');
+};
+
+export const dateFormatM = (date: string) => {
   return moment(date).format('M');
 };
 

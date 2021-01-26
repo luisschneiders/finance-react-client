c9f936061feb5cd8bc688b983bdec1b19d7a7609
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-27 07:39:32 +1100

    remove line

--- src/data/expenses/data.ts
+++ src/data/expenses/data.ts
@@ -3,7 +3,6 @@ import { Period } from '../../models/Period';
 import { fetchExpenses } from '../api/Expenses';
 
 export const fetchExpensesData = async (id: number, period: Period, params: string) => {
-
   const response: any = await fetchExpenses(id, period, params);
   const expenses = response as Expenses;
   const data:  any = {

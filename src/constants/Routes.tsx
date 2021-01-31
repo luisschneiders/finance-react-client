197cee3361a4b20681d57c014720b85613e15c4a
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-01-31 18:27:41 +1100

    code update

--- src/constants/Routes.tsx
+++ src/constants/Routes.tsx
@@ -14,14 +14,17 @@ export const REGISTER = '/register';
 export const SERVER = 'http://local.developing:3030';
 
 export const TABS = '/tabs';
-export const TAB1 = `${TABS}/tab1`;
-export const TAB2 = `${TABS}/tab2`;
-export const TAB3 = `${TABS}/tab3`;
+
 export const TABS_HOME = `${TABS}/home`;
-export const TABS_NEWS_DETAILS = `${TABS}/news-details`;
+
 export const TABS_EXPENSES = `${TABS}/expenses`;
+
+export const TABS_NEWS_DETAILS = `${TABS}/news-details`;
+
 export const TABS_TIMESHEETS = `${TABS}/timesheets`;
+
 export const TABS_TRANSACTIONS = `${TABS}/transactions`;
+
 export const TABS_TRIPS = `${TABS}/trips`;
 
 export const WELCOME = '/welcome';

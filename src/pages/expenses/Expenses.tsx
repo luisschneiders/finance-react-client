10314ae658c37bbbb9ff364834d9f22f7cac7df3
Luis Fernando Schneiders <schneiders.tech@gmail.com>
2021-02-03 07:15:29 +1100

    add modal and update styling for page Expenses - WIP

--- src/pages/expenses/Expenses.tsx
+++ src/pages/expenses/Expenses.tsx
@@ -1,10 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Expenses.scss';
 import {
+  IonButton,
   IonButtons,
+  IonDatetime,
+  IonFab,
+  IonFabButton,
+  IonFabList,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonLoading,
   IonMenuButton,
+  IonModal,
   IonPage,
+  IonSelect,
+  IonSelectOption,
   IonTitle,
   IonToolbar
 } from '@ionic/react';
@@ -27,6 +41,8 @@ import {
 import { Period } from '../../models/Period';
 import { setExpenses } from '../../data/expenses/expenses.actions';
 import LsMainExpenses from '../../components/expenses/MainExpenses';
+import { AppColor } from '../../enum/AppColor';
+import { add, close, ellipsisVertical, search } from 'ionicons/icons';
 
 interface StateProps {
   isLoggedIn: boolean;
@@ -55,6 +71,13 @@ const ExpensesPage: React.FC<ExpensesProps> = ({
   });
 
   const [params, setParams] = useState<string>('all');
+  const [showSearchModal, setShowSearchModal] = useState<boolean>(false);
+  const [showAddRecordModal, setShowAddRecordModal] = useState<boolean>(false);
+  const [busy, setBusy] = useState(false);
+
+  const [selectedStartDate, setSelectedStartDate] = useState<string>(startPeriod(MOMENT.currentMonthYYYMMDD));
+  const [selectedEndDate, setSelectedEndDate] = useState<string>(endPeriod(MOMENT.currentMonthYYYMMDD));
+  const [selectedExpenses, setSelectedExpenses] = useState<any[]>([]);
 
   const decreasePeriod = (date: Period = period) => {
     const newDate: Period = Object.assign({}, {
@@ -83,6 +106,27 @@ const ExpensesPage: React.FC<ExpensesProps> = ({
     setExpensesTimeTransition(newDate);
   };
 
+  const submitSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setBusy(true);
+    const newDate: Period = Object.assign({}, {
+      startDate: selectedStartDate,
+      endDate: selectedEndDate,
+    });
+    setPeriod(newDate);
+    setExpensesTimeTransition(newDate);
+    setShowSearchModal(false);
+    setBusy(false);
+
+  }
+
+  const submitAddRecord = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setBusy(true);
+    setShowAddRecordModal(false);
+  }
+
   if (isLoggedIn && userProfileServer) {
     if (period !== expensesTimeTransition) {
       setExpenses(userProfileServer.userId, period, params);
@@ -96,7 +140,16 @@ const ExpensesPage: React.FC<ExpensesProps> = ({
           <IonButtons slot="start">
             <IonMenuButton auto-hide="true"></IonMenuButton>
           </IonButtons>
-          <IonTitle className="ion-text-center">Expenses</IonTitle>
+          <IonTitle>Expenses</IonTitle>
+          <IonFab vertical="center" horizontal="end">
+            <IonFabButton color={AppColor.TERTIARY} size="small">
+              <IonIcon icon={ellipsisVertical} />
+            </IonFabButton>
+            <IonFabList side="start">
+              <IonFabButton><IonIcon color={AppColor.TERTIARY} icon={search} onClick={() => setShowSearchModal(true)} /></IonFabButton>
+              <IonFabButton><IonIcon color={AppColor.SUCCESS} icon={add} onClick={() => setShowAddRecordModal(true)} /></IonFabButton>
+            </IonFabList>
+          </IonFab>
         </IonToolbar>
         <IonToolbar>
           <LsTimeTransition
@@ -106,6 +159,78 @@ const ExpensesPage: React.FC<ExpensesProps> = ({
             increasePeriod={increasePeriod} />
         </IonToolbar>
       </IonHeader>
+      <IonLoading message="Please wait..." duration={0} isOpen={busy}></IonLoading>
+      <IonModal isOpen={showSearchModal} onDidDismiss={() => setShowSearchModal(false)}>
+        <form noValidate onSubmit={submitSearch}>
+          <IonList lines="full">
+            <IonListHeader>
+              <IonLabel>Search Expenses</IonLabel>
+              <IonButton onClick={() => setShowSearchModal(false)}>
+                <IonIcon icon={close} color={AppColor.TERTIARY}>Close</IonIcon>
+              </IonButton>
+            </IonListHeader>
+            <IonItem>
+              <IonLabel>From</IonLabel>
+              <IonDatetime displayFormat="MMM DD, YYYY" placeholder="Select Date" value={selectedStartDate} onIonChange={e => setSelectedStartDate(e.detail.value!)}></IonDatetime>
+            </IonItem>
+            <IonItem>
+              <IonLabel>To</IonLabel>
+              <IonDatetime displayFormat="MMM DD, YYYY" placeholder="Select Date" value={selectedEndDate} onIonChange={e => setSelectedEndDate(e.detail.value!)}></IonDatetime>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Expense</IonLabel>
+{/* TODO: Retrieve expenses type */}
+              <IonSelect multiple={true}>
+                <IonSelectOption value="brown">Diesel/Petrol</IonSelectOption>
+                <IonSelectOption value="blonde">Rent</IonSelectOption>
+                <IonSelectOption value="black">Food/Drinks</IonSelectOption>
+                <IonSelectOption value="red">Others</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem lines="none">
+              <div slot="end" className="ion-padding-vertical">
+                <IonButton type="submit" shape="round" color={AppColor.TERTIARY}>Search</IonButton>
+              </div>
+            </IonItem>
+          </IonList>
+        </form>
+      </IonModal>
+      <IonModal isOpen={showAddRecordModal} onDidDismiss={() => setShowAddRecordModal(false)}>
+        <form noValidate onSubmit={submitAddRecord}>
+        <IonList lines="full">
+          <IonListHeader>
+            <IonLabel>Add New Expense</IonLabel>
+            <IonButton onClick={() => setShowAddRecordModal(false)}>
+              <IonIcon icon={close} color={AppColor.TERTIARY}>Close</IonIcon>
+            </IonButton>
+          </IonListHeader>
+          <IonItem>
+            <IonLabel>Location</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Date</IonLabel>
+            <IonDatetime displayFormat="MMM DD, YYYY" placeholder="Select Date" value={selectedStartDate} onIonChange={e => setSelectedStartDate(e.detail.value!)}></IonDatetime>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Bank</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Expense</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Comments</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Amount</IonLabel>
+          </IonItem>
+          <IonItem lines="none">
+              <div slot="end" className="ion-padding-vertical">
+                <IonButton type="submit" shape="round" color={AppColor.SUCCESS}>Add Expense</IonButton>
+              </div>
+            </IonItem>
+        </IonList>
+        </form>
+      </IonModal>
       <LsMainExpenses></LsMainExpenses>
     </IonPage>
   );

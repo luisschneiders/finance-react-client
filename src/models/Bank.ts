a021726d3a802fcf143ea1e050cea623d34b49af
Luis Schneiders <schneiders.tech@gmail.com>
2021-03-21 19:04:25 +1100

    rename Bank modal

---
+++ src/models/Bank.ts
@@ -0,0 +1,18 @@
+import { Pagination } from './Pagination';
+
+export interface Bank {
+  bankId: number;
+  bankDescription: string;
+  bankAccount: string;
+  bankInitialBalance: number;
+  bankCurrentBalance: number;
+  bankIsActive: boolean;
+  bankInsertedBy: number;
+  bankCreatedAt: string;
+  bankUpdatedAt: string;
+}
+
+export interface BankList {
+  banks: Bank[];
+  pagination: Pagination;
+}

b8d2384555149f267210c5d22717f06605c0a6dd
Luis Schneiders <schneiders.tech@gmail.com>
2021-03-24 07:42:41 +1100

    update ExpenseType page with new modal

--- src/pages/expense-type/ExpenseType.tsx
+++ src/pages/expense-type/ExpenseType.tsx
@@ -1,25 +1,19 @@
 import {
-  IonButton,
   IonButtons,
   IonContent,
   IonFab,
   IonFabButton,
   IonHeader,
   IonIcon,
-  IonInput,
-  IonItem,
-  IonLabel,
   IonLoading,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar
 } from '@ionic/react';
-import React, { useEffect, useState } from 'react';
-import { toast } from '../../components/toast/Toast';
+import React, { useEffect } from 'react';
 import { connect } from '../../data/connect';
 import { AppColor } from '../../enum/AppColor';
-import { StatusColor } from '../../enum/StatusColor';
 import { UserProfileServer } from '../../models/UserProfileServer';
 import * as selectorsUser from '../../data/user/user.selectors';
 import * as selectorsSessions from '../../data/sessions/sessions.selectors';
@@ -27,24 +21,21 @@ import * as selectorsExpenseType from '../../data/expenseType/expenseType.select
 import { PageSize } from '../../enum/PageSize';
 import LsListItemExpenseType from '../../components/list/ListItemExpenseType';
 import {
-  addExpenseType,
   setExpenseTypeList
 } from '../../data/expenseType/expenseType.actions';
-import { ModalProvider } from '../../components/modal/ModalProvider';
 import { add } from 'ionicons/icons';
-import { useModal } from '../../hooks/useModal';
-import LsMainModal from '../../components/modal/MainModal';
+import { setModalExpenseTypeShow } from '../../data/modal/modal.actions';
+import LsModalExpenseType from '../../components/modal/ModalExpenseType';
 
 interface StateProps {
   isLoggedIn: boolean;
-  isSaving: boolean;
   isFetching: boolean;
   userProfileServer: UserProfileServer;
 }
 
 interface DispatchProps {
   setExpenseTypeList: typeof setExpenseTypeList;
-  addExpenseType: typeof addExpenseType;
+  setModalExpenseTypeShow: typeof setModalExpenseTypeShow;
 }
 
 interface ExpensesTypeProps extends StateProps, DispatchProps {}
@@ -52,35 +43,21 @@ interface ExpensesTypeProps extends StateProps, DispatchProps {}
 const ExpenseTypePage: React.FC<ExpensesTypeProps> = ({
   isLoggedIn,
   isFetching,
-  isSaving,
   userProfileServer,
   setExpenseTypeList,
-  addExpenseType,
+  setModalExpenseTypeShow
 }) => {
-  const { showModal, isSubmitting, handleShow, handleClose } = useModal();
-
-  const [expenseTypeDescription, setExpenseTypeDescription] = useState<string>('');
   
   useEffect(() => {
     if (isLoggedIn && userProfileServer) {
       setExpenseTypeList(userProfileServer.userId, 1, PageSize.S_12);
     }
-  }, [isLoggedIn, userProfileServer, setExpenseTypeList]);
-
-  const expenseTypeForm = async(e: React.FormEvent) => {
-    e.preventDefault();
-    
-    if (!expenseTypeDescription) {
-      return toast('Description is required!', StatusColor.WARNING);
-    }
-    addExpenseType({
-      expenseTypeDescription,
-      expenseTypeInsertedBy: userProfileServer.userId,
-      expenseTypeIsActive: true
-    });
-    setExpenseTypeDescription('');
-    handleClose();
-  }
+  }, [
+    isLoggedIn,
+    userProfileServer,
+    setExpenseTypeList,
+    setModalExpenseTypeShow,
+  ]);
 
   return (
     <IonPage>
@@ -94,7 +71,7 @@ const ExpenseTypePage: React.FC<ExpensesTypeProps> = ({
             <IonFabButton color={AppColor.TERTIARY} size="small" title="Add new record">
               <IonIcon 
                 icon={add}
-                onClick={() => handleShow()}
+                onClick={() => setModalExpenseTypeShow(true)}
                 size="small"
               />
             </IonFabButton>
@@ -104,41 +81,8 @@ const ExpenseTypePage: React.FC<ExpensesTypeProps> = ({
       <IonLoading message="Please wait..." duration={0} isOpen={isFetching}></IonLoading>
       <IonContent className="ion-padding">
         <LsListItemExpenseType />
+        <LsModalExpenseType />
       </IonContent>
-      <ModalProvider>
-      <LsMainModal
-        id="transaction-type"
-        show={showModal}
-        title="New expense category"
-        isSubmitting={isSubmitting}
-        closeModal={handleClose}
-      >
-          <form noValidate onSubmit={expenseTypeForm}>
-            <IonItem>
-              <IonLabel position="stacked">Description</IonLabel>
-              <IonInput
-                name="expenseTypeDescription"
-                type="text"
-                value={expenseTypeDescription} spellCheck={false} autocapitalize="off"
-                onIonChange={(e: any) => setExpenseTypeDescription(e.detail.value!)}
-                required
-              />
-            </IonItem>
-            <IonItem lines="none">
-              <div slot="end" className="ion-padding-vertical">
-                <IonButton
-                  type="submit"
-                  fill="outline"
-                  color={AppColor.SUCCESS}
-                  disabled={isSaving}
-                >
-                  Save
-                </IonButton>
-              </div>
-            </IonItem>
-          </form>
-      </LsMainModal>
-      </ModalProvider>
     </IonPage>
   );
 };
@@ -146,13 +90,12 @@ const ExpenseTypePage: React.FC<ExpensesTypeProps> = ({
 export default connect<{}, StateProps, DispatchProps>({
   mapStateToProps: (state) => ({
     isLoggedIn: selectorsUser.getIsLoggedIn(state),
-    isSaving: selectorsExpenseType.isSavingExpenseType(state),
     isFetching: selectorsExpenseType.isFetchingExpenseTypeList(state),
     userProfileServer: selectorsSessions.getUserProfileServer(state),
   }),
   mapDispatchToProps: ({
     setExpenseTypeList,
-    addExpenseType,
+    setModalExpenseTypeShow,
   }),
   component: React.memo(ExpenseTypePage)
 });
